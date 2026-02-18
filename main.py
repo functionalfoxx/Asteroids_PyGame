2582c69b79957c6833db4ecd33052d949f4fe4fd
functionalfoxx <sabrinakirin@gmail.com>
2026-02-18 13:44:48 -0800

    Draw player triangle on screen

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting Asteroids with pygame version:", pygame.__version__)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0   
 
     while True:
         log_state()
 
         screen.fill("black")
+
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,5 +30,7 @@ def main():
 
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
