fcb0deac3548b93e4fd70f1a00537af91fc4ef9f
functionalfoxx <functionalfoxx@users.noreply.github.com>
2026-02-18 15:55:43 -0800

    Player shots no longer constant

--- player.py
+++ player.py
@@ -5,7 +5,8 @@ from constants import (
     LINE_WIDTH,
     PLAYER_TURN_SPEED,
     PLAYER_SPEED,
-    PLAYER_SHOOT_SPEED
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN_SECONDS
 )
 from shot import Shot
 
@@ -13,6 +14,7 @@ class Player(CircleShape):
     def __init__ (self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -31,7 +33,12 @@ class Player(CircleShape):
         )
 
     def update(self, dt):
+
         keys = pygame.key.get_pressed()
+
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
@@ -53,6 +60,9 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return  
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
         new_shot = Shot(self.position.x, self.position.y)
         direction = pygame.Vector2(0, 1).rotate(self.rotation)
         new_shot.velocity = direction * PLAYER_SHOOT_SPEED
